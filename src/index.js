102e80dff16d3a324a01e867782246f8b9da1ee2
Matt Haffner <mrhaffner13@gmail.com>
2020-10-14 19:51:55 -0500

    added delete buttons to sidebar that deletes proj from obj, sidebarDOM, and clears the main if that project was open

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-// ‘todos’ are going to be objects that you’ll want to dynamically create, which means either using factories or constructors/classes to generate them.
-
 //todo properties
     //minimum: title, description, dueDate and priority
         //extra: notes and checklist
 
-//todos should be separated into projects
-    //there should be a deault project when the user opens the app
-    //Users should be able to create new projects and choose which project their todos go into.
-
 // separate your application logic (i.e. creating new todos, setting todos as complete, changing todo priority etc.) from the DOM-related stuff, so keep all of those things in separate modules.
 
 //UI
-    //view all projects
-    //view all todos in each project (probably just the title and duedate.. perhaps changing color for different priorities)
     //expand a single todo to see/edit its details
     //delete a todo
 
@@ -21,14 +13,12 @@
 //Since you are probably already using webpack, adding external libraries from npm is a cinch! You might want to consider using the following useful library in your code:
     //date-fns gives you a bunch of handy functions for formatting and manipulating dates and times.
 
-
 //strucure:
     //index file holds the basic sidebar
     //another file holds what is loaded in the sidebar
     //another holds what is loads in current project
 
 
-//module to hold array that contains project objects
 const projectsList = (() => {
     const projectArr = [
         {
@@ -51,26 +41,21 @@ const projectsList = (() => {
     return { projectArr, currentId };
 })();
 
-//factory function to create project objects
 const Project = (name, id) => {
     const projId = id;
     const currentTaskId = 0;
     const tasks = [];
-        //should you be able to change task? probably not, will change later
     return { name, projId, currentTaskId, tasks };
 };
-//function to add project object to project array
+
 function addProject(project) {
     projectsList.projectArr.push(project)
 }
 
-//factory function to create task objects 
 const Task = (name, projId) => {
     const getProjId = () => projId;
     return { name, getProjId };
 };
-//function to add task objects to an array inside of the correct project object
-
 
 
 //create a pop up form in html (then maybe eventually dynamically generate it with Javascript on new project button click)
@@ -78,7 +63,6 @@ const Task = (name, projId) => {
 const submitBtn = document.getElementById('proj_form_submit_btn');
 submitBtn.addEventListener('click', () => {
     const inputName = document.getElementById('proj_title_input').value;
-    // let inputId = projectsList.projectArr.length; /*This needs to change to be generated as a number 1 higher than the highest id in array once a functional delete project button is created */
     let inputId = projectsList.currentId
     projectsList.currentId ++;
     projectsList.projectArr.push(Project(inputName, inputId));
@@ -90,13 +74,27 @@ function addProjectSide(name, id) {
     const sideNode = document.getElementById('side_proj_div');
     let newDiv = document.createElement('div')
     newDiv.id = `side_${id}_div`
-    let newProject = `<h3 id=side_${id}_title">${name}</h3>`;
+    let newProject = 
+        `<h3 id=side_${id}_title">${name}</h3>
+        <button id="side_${id}_delete">Delete</button>`;
     newDiv.innerHTML = newProject;
     sideNode.appendChild(newDiv);
     addProjectBtn(newDiv.firstChild, name, id); //might need to change this to something other than .firstChild when you reorganize the DOM tree
+    addDeleteProj(id, newDiv)
     }; 
 
-//this will need to run when a new project is created, but will also need to be applied to the Default Project
+function addDeleteProj(id, parent) {
+    const dBtn = document.getElementById(`side_${id}_delete`)
+    dBtn.addEventListener('click', () => {
+        let objIndex = projectsList.projectArr.findIndex((x) => x === +id);
+        projectsList.projectArr.slice(objIndex, 1);
+        parent.remove();
+        if (document.getElementById(`${id}_proj`)) {
+            clearMain();
+        };
+    })
+}
+
 function addProjectBtn(proj, name, id) {
     proj.addEventListener('click', () => {
         clearMain();
@@ -128,8 +126,6 @@ function addProjectMain(name, id) { /*get rid of the (name/id dependencies here 
             </div>
         </div>`
     projMain.innerHTML = projectContents
-    //function to loop through appropriate object and add all the task
-    //find the object with the same id
     const taskContainer = document.getElementById(`${id}_proj_task_div`);
     let currentProject = projectsList.projectArr.filter(obj => obj.projId === +id)[0];
     currentProject.tasks.forEach(task => addTaskDOM(task.name, task.taskId, id, taskContainer));
@@ -145,38 +141,22 @@ function addTaskDOM (name, id, pId, parent) {
 };
 
 
-//add event listener to the btn that inputs the task in the current objects task array (runs as part of addProjectMain())
 function addTaskBtn(id, task) {
     const taskBtn = document.getElementById(`${id}_proj_btn`)
     taskBtn.addEventListener('click', () => {
-        console.log('hi')
-        //get value from input
         const inputText = document.getElementById(`${id}_proj_task_input`).value;
-        //add that input to the 
         let projIndex = projectsList.projectArr.findIndex((x) => x.projId === +id);
         let addId = projectsList.projectArr[projIndex].currentTaskId;
         projectsList.projectArr[projIndex].currentTaskId ++;
         projectsList.projectArr[projIndex].tasks.push(Task(inputText, addId));
-        //run function that writes that task to the DOM
         addTaskDOM(inputText, addId, id, task);
     })
 }
-//create function that then writes that task to the DOM
-// function addTaskDOM(name, id) {
 
-// }
 
-// function addProjectSide(name, id) {
-//     //maybe refactor this once everything is done so that anything coming from the form is appended, but everything else can be with innerHTML
-//     const sideNode = document.getElementById('side_proj_div');
-//     let newDiv = document.createElement('div')
-//     newDiv.id = `side_${id}_div`
-//     let newProject = `<h3 id=side_${id}_title">${name}</h3>`;
-//     newDiv.innerHTML = newProject;
-//     sideNode.appendChild(newDiv);
-//     addProjectBtn(newDiv.firstChild, name, id); //might need to change this to something other than .firstChild when you reorganize the DOM tree
-//     }; 
 
-let defaultProj = document.getElementById('side_default_proj_title');
 addProjectMain('Default Project', 0);
+let defaultProj = document.getElementById('side_0_title');
 addProjectBtn(defaultProj, 'Default Project', 0); /*probably make this so it get from the object or not*/
+addDeleteProj(0, defaultProj.parentElement)
+//maybe make a function that does all this shit/make the default proj in sidebar be generated on load
